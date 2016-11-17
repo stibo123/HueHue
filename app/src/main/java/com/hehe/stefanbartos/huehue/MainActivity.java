482d934d1cca8f6b70b0f40b4ba6d3f88de1874c
stibo123 <stibo123@gmail.com>
2016-11-17 09:14:33 +0100

    First Commit

---
+++ app/src/main/java/com/hehe/stefanbartos/huehue/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hehe.stefanbartos.huehue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
